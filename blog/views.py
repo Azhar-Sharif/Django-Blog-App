e3fd82e1abe96935f646f7a055b5dd34e98eb28a
Azhar-Sharif <azharsharif042@gmail.com>
2025-09-08 14:05:42 +0500

    Update view to fetch Post data, render in template, and add Django Admin

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from .models import *
 # Create your views here.
 
 def hello_view(request):
     return HttpResponse("Hello Django")
 
 def home_view(request):
-    context = {"name":"Azhar","current_date": datetime.now() }
+    posts = Post.objects.all()
+    context = {"name":"Azhar","current_date": datetime.now(),"posts" : posts }
     return render(request, "blog/home.html",context)
