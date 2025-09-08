2e12aff80611f8665eacfd22d97cd36f39929e68
Azhar-Sharif <azharsharif042@gmail.com>
2025-09-08 14:28:37 +0500

    Add Comment model, QuerySets, and display posts with comments in template

--- blog/models.py
+++ blog/models.py
@@ -8,3 +8,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,
+    related_name="comments")
+    content = models.TextField()
+
+    def __str__(self):
+        return f"Comment on{self.post.title}"
--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,6 @@ def hello_view(request):
     return HttpResponse("Hello Django")
 
 def home_view(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().prefetch_related("comments")
     context = {"name":"Azhar","current_date": datetime.now(),"posts" : posts }
     return render(request, "blog/home.html",context)
